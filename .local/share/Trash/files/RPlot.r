39dd41ef7fc081213d17d0696dadef4a854a537d
Gilian Gunsch <gunsch.3@osu.edu>
2021-07-08 02:07:06 +0000

    updated

---
+++ .local/share/Trash/files/RPlot.r
@@ -0,0 +1,15 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+data=read.csv(agrs[1])
+
+plot(x = data$x,y = data$y, xlab = "x axis", ylab = "y axis", main = "Plot")
+
+dev.copy(png,'orig.png')
+dev.off()
+
+abline(lm(y ~ x, data), col = "blue")
+
+dev.copy(png,'lm.png')
+dev.off()
+
+
